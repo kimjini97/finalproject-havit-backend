183358d1404788d14e271b2f7f8b5974e60e0dc8
namuneo <rlatjdwns8934@gmail.com>
2022-09-19 07:53:59 +0900

    cors setting, env setting, member setting

---
+++ src/main/java/com/ahnhaetdaeyo/finalbe/entity/Member.java
@@ -0,0 +1,34 @@
+package com.ahnhaetdaeyo.finalbe.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Member{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column
+    private String imgUrl;
+
+
+}
