9dce219c430c21bd0e56b40295d005da92750478
kimjini97 <woaa_@naver.com>
2022-09-22 12:35:00 +0900

    [Feat] 인증샷 생성, 수정, 삭제 기능 구현

--- src/main/java/com/havit/finalbe/controller/CertifyController.java
+++ src/main/java/com/havit/finalbe/controller/CertifyController.java
@@ -1,16 +1,24 @@
 package com.havit.finalbe.controller;
 
+import com.havit.finalbe.dto.request.CertifyRequestDto;
 import com.havit.finalbe.dto.response.ResponseDto;
+import com.havit.finalbe.service.CertifyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 public class CertifyController {
 
+    private final CertifyService certifyService;
+
     @PostMapping("/api/auth/certify")
-    public ResponseDto<?> createCertify() {
-        return null;
+    public ResponseDto<?> createCertify(@ModelAttribute CertifyRequestDto certifyRequestDto,
+                                        HttpServletRequest request) throws IOException {
+        return certifyService.createCertify(certifyRequestDto, request);
     }
 
     @GetMapping("/api/auth/certify/{certifyId}")
@@ -19,12 +27,13 @@ public class CertifyController {
     }
 
     @PutMapping("/api/auth/certify/{certifyId}")
-    public ResponseDto<?> updateCertify(@PathVariable Long certifyId) {
-        return null;
+    public ResponseDto<?> updateCertify(@PathVariable Long certifyId, @ModelAttribute CertifyRequestDto certifyRequestDto,
+                                        HttpServletRequest request) throws IOException {
+        return certifyService.updateCertify(certifyId, certifyRequestDto, request);
     }
 
     @DeleteMapping("/api/auth/certify/{certifyId}")
-    public ResponseDto<?> deleteCertify(@PathVariable Long certifyId) {
-        return null;
+    public ResponseDto<?> deleteCertify(@PathVariable Long certifyId, HttpServletRequest request) {
+        return certifyService.deleteCertify(certifyId, request);
     }
 }
