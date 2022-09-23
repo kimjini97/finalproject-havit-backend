2e65bfbbcd2a98fdcce7c09bd7b2ace455ae07a1
kimjini97 <woaa_@naver.com>
2022-09-23 23:50:43 +0900

    [22-09-23] 그룹 참여하기 기능 구현

---
+++ src/main/java/com/havit/finalbe/controller/ParticipateController.java
@@ -0,0 +1,33 @@
+package com.havit.finalbe.controller;
+
+import com.havit.finalbe.dto.response.ResponseDto;
+import com.havit.finalbe.service.ParticipateService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Tag(name = "[참여하기 API]")
+@RestController
+@RequiredArgsConstructor
+public class ParticipateController {
+
+    private final ParticipateService participateService;
+
+    @Operation(summary = "그룹 참여하기", description = "해당하는 그룹의 참여 이력을 확인하고 참여합니다.")
+    @PostMapping("/api/auth/participate/{groupId}")
+    public ResponseDto<?> participate(@PathVariable Long groupId, HttpServletRequest request) {
+        return participateService.participate(groupId, request);
+    }
+
+    @Operation(summary = "그룹 참여 취소하기", description = "해당하는 그룹의 참여 이력을 확인하고 취소합니다.")
+    @DeleteMapping("/api/auth/participate/{groupId}")
+    public ResponseDto<?> cancelParticipation(@PathVariable Long groupId, HttpServletRequest request) {
+        return participateService.cancelParticipation(groupId, request);
+    }
+}
---
+++ src/main/java/com/havit/finalbe/service/ParticipateService.java
@@ -0,0 +1,129 @@
+package com.havit.finalbe.service;
+
+import com.havit.finalbe.dto.response.GroupResponseDto;
+import com.havit.finalbe.dto.response.ResponseDto;
+import com.havit.finalbe.entity.Certify;
+import com.havit.finalbe.entity.Groups;
+import com.havit.finalbe.entity.Member;
+import com.havit.finalbe.entity.Participate;
+import com.havit.finalbe.repository.CertifyRepository;
+import com.havit.finalbe.repository.ParticipateRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.havit.finalbe.exception.ErrorMsg.*;
+
+@RequiredArgsConstructor
+@Service
+public class ParticipateService {
+
+    private final ParticipateRepository participateRepository;
+    private final CertifyRepository certifyRepository;
+    private final GroupService groupService;
+    private final ServiceUtil serviceUtil;
+
+    public ResponseDto<?> participate(Long groupId, HttpServletRequest request) {
+
+        if (null == request.getHeader("Refresh-Token")) {
+            return ResponseDto.fail(INVALID_LOGIN);
+        }
+
+        if (null == request.getHeader("Authorization")) {
+            return ResponseDto.fail(INVALID_LOGIN);
+        }
+
+        Member member = serviceUtil.validateMember(request);
+        if (null == member) {
+            return ResponseDto.fail(INVALID_TOKEN);
+        }
+
+        Groups groups = groupService.isPresentGroup(groupId);
+        if (null == groups) {
+            return ResponseDto.fail(GROUP_NOT_FOUND);
+        }
+
+        if (participateRepository.findByGroupsAndMember(groups, member).isEmpty()) {
+            Participate participate = Participate.builder()
+                    .groups(groups)
+                    .member(member)
+                    .build();
+            participateRepository.save(participate);
+
+            // 태그 가져오기
+            List<String> tagListByGroup = serviceUtil.getTagNameListFromGroupTag(groups);
+
+            // 참여한 멤버 카운팅
+            int memberCount = participateRepository.countByGroups_GroupId(groupId);
+
+            // 참여 멤버 목록
+            List<Participate> participateList = participateRepository.findAllByGroups(groups);
+            List<Member> memberList = new ArrayList<>();
+            for (Participate participateMember : participateList) {
+                memberList.add(participateMember.getMember());
+            }
+
+            // 인증샷 이미지 URL 목록 가져오기
+            List<Certify> certifyList = certifyRepository.findByGroups_GroupId(groupId);
+            List<String> certifyImgUrlList = new ArrayList<>();
+            for (Certify imgUrl : certifyList) {
+                certifyImgUrlList.add(imgUrl.getImgUrl());
+            }
+
+            return ResponseDto.success(
+                    GroupResponseDto.builder()
+                            .groupId(groupId)
+                            .title(groups.getTitle())
+                            .nickname(groups.getMember().getNickname())
+                            .leaderName(groups.getLeaderName())
+                            .crewName(groups.getCrewName())
+                            .startDate(groups.getStartDate())
+                            .content(groups.getContent())
+                            .imgUrl(groups.getImgUrl())
+                            .createdAt(groups.getCreatedAt())
+                            .modifiedAt(groups.getModifiedAt())
+                            .groupTag(tagListByGroup)
+                            .memberCount(memberCount)
+                            .memberList(memberList)
+                            .certifyImgUrlList(certifyImgUrlList)
+                            .build()
+            );
+        }
+        return ResponseDto.fail(DUPLICATE_PARTICIPATION);
+    }
+
+    public ResponseDto<?> cancelParticipation(Long groupId, HttpServletRequest request) {
+
+        if (null == request.getHeader("Refresh-Token")) {
+            return ResponseDto.fail(INVALID_LOGIN);
+        }
+
+        if (null == request.getHeader("Authorization")) {
+            return ResponseDto.fail(INVALID_LOGIN);
+        }
+
+        Member member = serviceUtil.validateMember(request);
+        if (null == member) {
+            return ResponseDto.fail(INVALID_TOKEN);
+        }
+
+        Groups groups = groupService.isPresentGroup(groupId);
+        if (null == groups) {
+            return ResponseDto.fail(GROUP_NOT_FOUND);
+        }
+
+        if (participateRepository.findByGroupsAndMember(groups, member).isPresent()) {
+            Participate participate = Participate.builder()
+                    .groups(groups)
+                    .member(member)
+                    .build();
+            participateRepository.delete(participate);
+            return ResponseDto.success("참여 취소가 완료되었습니다.");
+        }
+        return ResponseDto.fail(PARTICIPATION_NOT_FOUND);
+    }
+}
