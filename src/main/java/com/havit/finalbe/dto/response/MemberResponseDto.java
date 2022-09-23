44e8017ada6ad937a9c46439a9e1c55b8db54cca
namuneo <rlatjdwns8934@gmail.com>
2022-09-23 11:23:18 +0900

    swagger 완료

--- src/main/java/com/havit/finalbe/dto/response/MemberResponseDto.java
+++ src/main/java/com/havit/finalbe/dto/response/MemberResponseDto.java
@@ -8,23 +8,25 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Schema(description = "멤버 ResponseDto")
+@Schema(description = "멤버 응답Dto")
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberResponseDto {
 
-    @Schema(description = "멤버 고유 id(PK)")
-    private Long id;
+    @Schema(description = "멤버 id", example = "1")
+    private Long memberId;
     
     @Schema(description = "이메일", example = "test@gmail.com")
     private String username;
     
-    @Schema(description = "닉네임", example = "루피짱")
+    @Schema(description = "닉네임", example = "김병처리")
     private String nickname;
 
+    @Schema(description = "가입 일시", example = "2022-07-25T12:43:01.226062")
     private LocalDateTime createdAt;
 
+    @Schema(description = "수정 일시", example = "2022-07-25T12:43:01.226062")
     private LocalDateTime modifiedAt;
 }
