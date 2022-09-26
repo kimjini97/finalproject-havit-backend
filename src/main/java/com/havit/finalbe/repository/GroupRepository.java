6522659c90ce166909c857ad717207b5832fa75a
kimjini97 <woaa_@naver.com>
2022-09-26 15:37:47 +0900

    [22-09-26] 검색 기능 구현

--- src/main/java/com/havit/finalbe/repository/GroupRepository.java
+++ src/main/java/com/havit/finalbe/repository/GroupRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface GroupRepository extends JpaRepository<Groups, Long> {
     List<Groups> findAllByOrderByCreatedAtDesc();
+    List<Groups> findAllByTitleContainingIgnoreCaseOrMember_NicknameContainingIgnoreCase(String title, String nickname);
 }
