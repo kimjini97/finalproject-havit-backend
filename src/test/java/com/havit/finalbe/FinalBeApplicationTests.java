7035030daafc0cb4da7f432ac5ab07f0dee191bf
namuneo <rlatjdwns8934@gmail.com>
2022-10-11 22:10:54 +0900

    Update FinalBeApplicationTests.java

--- src/test/java/com/havit/finalbe/FinalBeApplicationTests.java
+++ src/test/java/com/havit/finalbe/FinalBeApplicationTests.java
@@ -3,7 +3,7 @@ package com.havit.finalbe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class FinalBeApplicationTests {
 
     @Test
